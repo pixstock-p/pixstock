d59cc2e3d98c8a8261329c916a811a3879012339
pixstock.p <pixstock.p@gmail.com>
2018-01-16 22:48:38 +0900

    タスクランナーを整理

--- buildtool/gulpfile.babel.js
+++ buildtool/gulpfile.babel.js
@@ -3,20 +3,28 @@ import exec from 'gulp-exec';
 var options = { continueOnError: false, pipeStdout: false, };
 var reportOptions = { err: false, stderr: true, stdout: true };
 
+/**
+ * PixstockAppのAngular2が使用する共通ライブラリのビルド
+ */
 gulp.task('build_angular_lib', () => {
     return gulp.src('gulpfile.babel.js')
         .pipe(exec('ngc -p ../src/PixstockApp/Pixstock.Nc.App.Core', options))
         .pipe(exec.reporter(reportOptions));
 });
 
+/**
+ * PixstockAppのAngular2モジュールのビルド
+ */
 gulp.task('build_angular_app', () => {
     return gulp.src('gulpfile.babel.js')
+        .pipe(exec('npm run install --prefix ./../src/PixstockApp/Pixstock.Nc.App.Pioneer', options))
+        .pipe(exec('npm run install --prefix ./../src/PixstockApp/Pixstock.Nc.Stella', options))
         .pipe(exec('npm run ng build --prefix ./../src/PixstockApp/Pixstock.Nc.App.Pioneer', options))
         .pipe(exec('npm run ng build --prefix ./../src/PixstockApp/Pixstock.Nc.Stella', options))
         .pipe(exec.reporter(reportOptions));
 });
 
-gulp.task('npm_install', () => {
+gulp.task('setup_pixstock', () => {
     return gulp.src('gulpfile.babel.js')
         .pipe(exec('npm install --prefix ./../src/PixstockApp/Pixstock.Nc.App.Core', options))
         .pipe(exec.reporter(reportOptions));
